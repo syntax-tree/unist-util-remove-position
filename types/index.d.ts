b7db55344215acd2281dfd6410d1005cff0389d3
Murderlon <merlijn@soverin.net>
2020-07-14 20:24:58 +0200

    use `Omit` instead of rebuilding `Node`

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,17 +1,13 @@
 // TypeScript Version: 3.5
 
-import {Node, Data} from 'unist'
+import {Node} from 'unist'
 
 declare namespace unistUtilRemovePosition {
   interface NodeWithUndefinedPosition extends Node {
     position: undefined
   }
 
-  interface NodeWithoutPosition {
-    [key: string]: unknown
-    type: string
-    data?: Data
-  }
+  interface NodeWithoutPosition extends Omit<Node, 'position'> {}
 }
 
 /**
