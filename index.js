0262f8e54c8a28138bd8bab2d408d6aa26874b60
Titus Wormer <tituswormer@gmail.com>
2022-06-01 18:30:37 +0200

    Remove unneeded overloading

--- index.js
+++ index.js
@@ -5,41 +5,30 @@
 import {visit} from 'unist-util-visit'
 
 /**
- * Utility to remove positions from a tree
+ * Remove the `position` field from a tree.
  *
- * @param node The unist tree
- * @param force if `force` is given, uses `delete`, otherwise, sets positions to `undefined`.
- * @returns The same node, but either with `position: undefined` or w/o `position` fields
+ * @template {Node} Tree
+ * @param {Tree} node
+ *   The unist tree.
+ * @param {boolean} [force=false]
+ *   If `force` is given (`boolean`, default: `false`), uses `delete` to remove
+ *   the field entirely, otherwise it’s set to `undefined`.
+ * @returns {Tree}
+ *   The given, modified, `node`.
  */
-export const removePosition =
+export function removePosition(node, force) {
+  visit(node, remove)
+
+  return node
+
   /**
-   * @type {(
-   *   (<Tree extends Node>(tree: Tree, force?: false) => Tree) &
-   *   (<Tree extends Node>(tree: Tree, force: true) => Tree)
-   * )}
+   * @param {Node} node
    */
-  (
-    /**
-     * @template {Node} Tree
-     * @param {Tree} node
-     * @param {boolean} [force=false]
-     * @returns {Tree}
-     */
-    function (node, force) {
-      visit(node, remove)
-
-      // @ts-ignore hush TS, we know what we’re doing.
-      return node
-
-      /**
-       * @param {Node} node the unist tree
-       */
-      function remove(node) {
-        if (force) {
-          delete node.position
-        } else {
-          node.position = undefined
-        }
-      }
+  function remove(node) {
+    if (force) {
+      delete node.position
+    } else {
+      node.position = undefined
     }
-  )
+  }
+}
